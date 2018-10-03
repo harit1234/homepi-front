1b1e23e39f5516cef41a35ae86f14b5571b056a7
harit1234 <hrtmeans@gmail.com>
2018-10-03 18:55:45 +0530

    another remote

--- home-pi/src/app/round-table/hc/hc.component.ts
+++ home-pi/src/app/round-table/hc/hc.component.ts
@@ -5,17 +5,17 @@ import { KnightpermitService } from '../../knightpermit.service'
 @Component({
   selector: 'app-hc',
   templateUrl: './hc.component.html',
-  styleUrls: ['./hc.component.css']
+  styleUrls: ['./hc.component.css'] 
 })
 export class HcComponent {
 
   constructor(private _http : HttpClient, private kp:KnightpermitService) { }
   headers = new HttpHeaders
   switchon(){
-    this._http.post('192.168.1.5:3000', {buttonStatus : 'true'},{headers:this.headers.set('token',this.kp.token)})
+    this._http.post('http://raspberrypi.local:3000', {buttonStatus : 'true'},{headers:this.headers.set('token',this.kp.token)})
   }
   switchoff(){
-    this._http.post('192.168.1.5:3000', {buttonStatus : 'false'},{headers:this.headers.set('token',this.kp.token)})
+    this._http.post('http://raspberrypi.local:3000', {buttonStatus : 'false'},{headers:this.headers.set('token',this.kp.token)})
   }
  
 }
